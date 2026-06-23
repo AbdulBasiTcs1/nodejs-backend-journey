64976e8f7ac9a6bd4b3dc534fbddc26199079195
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-06-23 16:01:07 +0500

    handled the POST and GET url

--- URL-Shortner/controllers/url.js
+++ URL-Shortner/controllers/url.js
@@ -1,8 +1,9 @@
-const { nanoId } = require("nanoid");
+// const shortid = require("shortid");
 const URL = require("../models/url");
+const shortid = require("shortId");
 
 async function handleGenerateNewShortUrl(req,res) {
-    const shortId = nanoId(8);
+    const shortId = shortid(8);
     const body = req.body
     if(!body.URL) return res.status(400).json({error:"url is required"})
 
--- URL-Shortner/index.js
+++ URL-Shortner/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const urlRoute = require("./routes/url")
 const { connectToMongoDB } = require("./connect");
 
+const URL = require("./models/url")
 const app = express();
 const PORT = 8001;
 
@@ -9,8 +10,26 @@ connectToMongoDB("mongodb://localhost:27017/short-url").then(() => {
     console.log("MongoDB connected!")
 })
 
-app.use("/",urlRoute);
+app.use(express.json());
 
-app.listen(PORT,()=>{
+app.use("/", urlRoute);
+
+app.get("/shortId", async (req, res) => {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({
+        shortId
+    }, {
+        $push: {
+            visitHistory: {
+                timestamp: Date.now(),
+            }
+        },
+    })
+
+    res.redirect(entry.redirectURL)
+})
+
+
+app.listen(PORT, () => {
     console.log(`Server is connected on PORT: ${PORT}`)
 })
